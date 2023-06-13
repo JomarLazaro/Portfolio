11a628bea128bdd881a76298b1c2df5e7740740e
Comsci Jomar <Jomarlazaro0806@gmail.com>
2023-06-13 20:23:12 +0800

    skeleton loading

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
+const allSkeleton = document.querySelectorAll('.skeleton')
 
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
@@ -54,6 +55,11 @@ if (isMobile) {
   });
 }
 
+window.addEventListener('load', function() {
+  allSkeleton.forEach(item=> {
+    item.classList.remove('skeleton')
+  })
+})
 
 links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
