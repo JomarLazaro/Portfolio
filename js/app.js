be8482f6e14f4fb966a66544c743738c79041f17
Comsci Jomar <Jomarlazaro0806@gmail.com>
2023-06-13 12:54:56 +0800

    Navigation Update

--- js/app.js
+++ js/app.js
@@ -40,6 +40,15 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+window.addEventListener('scroll', function() {
+  var navbar = document.getElementById('navbar');
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
 links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
 filter_btns.forEach((btn) =>
